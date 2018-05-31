9e5e755827e54cb9a802bde09bb0d63e61e40b71
Venu Ancha <vancha@hmcts.net>
2018-05-31 14:45:21 +0100

    revert back changes

--- src/test/java/uk/gov/hmcts/probate/SolCCDServiceAuthTokenGenerator.java
+++ src/test/java/uk/gov/hmcts/probate/SolCCDServiceAuthTokenGenerator.java
@@ -67,21 +67,14 @@ public class SolCCDServiceAuthTokenGenerator {
     }
 
     public String getUserId() {
-
-        String userid_local= RestAssured.given()
-                .header("Authorization", userToken)
-                .get(idamUserBaseUrl+"/details")
-                .body()
-                .path("id");
-//        https://idam-api-idam-saat.service.core-compute-idam-saat.internal/details
-//      //  String jwt = userToken.replaceFirst("Bearer ", "");
-//        Map<String, Object> claims;
-//        try {
-//            claims = JWTParser.parse(userToken).getJWTClaimsSet().getClaims();
-//        } catch (ParseException e) {
-//            throw new IllegalStateException("Cannot find user from authorization token ", e);
-//        }
-//        String userid_local = (String) claims.get("authGrantId");
+        String jwt = userToken.replaceFirst("Bearer ", "");
+        Map<String, Object> claims;
+        try {
+            claims = JWTParser.parse(jwt).getJWTClaimsSet().getClaims();
+        } catch (ParseException e) {
+            throw new IllegalStateException("Cannot find user from authorization token ", e);
+        }
+        String userid_local = (String) claims.get("id");
         System.out.println("userid_local...." + userid_local);
         return userid_local;
 
@@ -91,115 +84,32 @@ public class SolCCDServiceAuthTokenGenerator {
     private void createUserInIdam() {
 
         idamUsername = "simulate-delivered" + UUID.randomUUID() + "@notifications.service.gov.uk";
-        idamPassword =  "Venu1234";
-        System.out.println("idam Password..." + idamPassword);
-       Response res= RestAssured.given()
+        idamPassword = "Test123456";
+        RestAssured.given()
                 .header("Content-Type", "application/json")
                 .body("{\"email\":\"" + idamUsername + "\", \"forename\":\"Test\",\"surname\":\"User\",\"password\":\"" + idamPassword + "\"}")
                 .post(idamCreateUrl());
-
-        System.out.println("Idamcreateurl...." + idamCreateUrl());
-
-        System.out.println("response status code from create user..." + res.getStatusCode());
-        System.out.println("response from create user..." + res.getBody().prettyPrint());
         System.out.println("in create useridam ");
     }
 
 
     public String generateUserTokenWithNoRoles() {
         createUserInIdam();
-//        Map<String, String >m = new HashMap();
-//        m.put("username", "vm1234567@gmail.com");
-//        m.put("password", "Vm123456");
+        System.out.println("created user in idam");
+        final String encoded = Base64.getEncoder().encodeToString((idamUsername + ":" + idamPassword).getBytes());
+        final String redirectUriEnv = environment.equalsIgnoreCase("saat") == true
+                ? redirectUri
+                : "https://www.preprod.ccd.reform.hmcts.net/oauth2redirect";
+        final String token = RestAssured.given().baseUri(idamUserBaseUrl)
+                .header("Authorization", "Basic " + encoded)
+                .post("/oauth2/authorize?response_type=token&client_id=divorce&redirect_uri=" +
+                        redirectUriEnv)
+                .body()
+                .path("access-token");
+        System.out.println("token generated.."+token);
 
-//        given().urlEncodingEnabled(true)
-//                .param("username", SMOKE_TEST_USER)
-//                .param("password", SMOKE_TEST_USER_PASSWORD)
-//                .header("Accept", ContentType.JSON.getAcceptHeader()).post()
-
-       final String token = RestAssured.given()
-                        .param("username", "vm1234567@gmail.com")
-                        .param("password","Vm123456" )
-                              .post(idamUserBaseUrl+"/loginUser")
-                              .body()
-                              .path("access_token");
-
-
-//        System.out.println("created user in idam");
-//        final String encoded = Base64.getEncoder().encodeToString((idamUsername + ":" + idamPassword).getBytes());
-//        System.out.println("encoded auth is.." + encoded);
-//        final String redirectUriEnv = environment.equalsIgnoreCase("saat") == true
-//                ? redirectUri
-//                : "https://www.preprod.ccd.reform.hmcts.net/oauth2redirect";
-////        Map<String, String> header = new HashMap<>();
-////        header.put("username", idamUsername);
-////        header.put("password", idamPassword);
-//        Response res1
-//                = RestAssured.given().baseUri(idamUserBaseUrl)
-//                .header("Authorization",  "Basic "+encoded)
-//               // .headers(header)
-//                .post("/oauth2/authorize?username=vm1234567@gmail.com&password=Vm123456&response_type=token&client_id=probate&redirect_uri=https://www-test.probate.reform.hmcts.net/");
-//        System.out.println("res1 status code..." + res1.getStatusCode());
-//        System.out.println("res1 body.." + res1.getHeader("Location"));
-//
-////                .body()
-////                .path("access-token");
-////        System.out.println("token generated.."+token);
-//
-       userToken = "Bearer "+token;
-        System.out.println("token generated.."+userToken);
-////        return userToken;
-////        userToken = generateClientToken();
-
+        userToken = "Bearer " + token;
+        System.out.println("Usertoken generated..." + userToken);
         return userToken;
     }
-
-
-//    private String generateClientToken() {
-//        String code = generateClientCode();
-//        String token = "";
-//
-//        String jsonResponse = post(baseServiceOauth2Url + "/oauth2/token?code=" + code +
-//                "&client_secret=secret/test/ccidam/idam-api/oauth2/client-secrets/probate"+
-//                "&client_id=probate"+
-//                "&redirect_uri=https://www-test.probate.reform.hmcts.net"+
-//                "&grant_type=authorization_code")
-//                .body().asString();
-//
-//        ObjectMapper mapper = new ObjectMapper();
-//
-//        try {
-//            token = mapper.readValue(jsonResponse, ClientAuthorizationResponse.class).accessToken;
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
-//
-//        System.out.println("Generated user token...." + "Bearer "+token );
-//
-//        return "Bearer" +token;
-//    }
-//
-//    private String generateClientCode() {
-//        String code = "";
-//        createUserInIdam();
-//        System.out.println("created user in idam");
-//        final String encoded = Base64.getEncoder().encodeToString((idamUsername + ":" + idamPassword).getBytes());
-//        System.out.println("encoded auth is.." + encoded);
-//        String jsonResponse = given()
-//                .header("Authorization", "Basic "+encoded)
-//                .post(baseServiceOauth2Url + "/oauth2/authorize?response_type=code" +
-//                        "&client_id=probate"+
-//                        "&redirect_uri=https://www-test.probate.reform.hmcts.net")
-//                .asString();
-//
-//        ObjectMapper mapper = new ObjectMapper();
-//
-//        try {
-//            code = mapper.readValue(jsonResponse, ClientAuthorizationCodeResponse.class).code;
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
-//        System.out.println("Generated code..." + code);
-//        return code;
-//    }
 }
